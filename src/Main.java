48f1cef90748285c10d7971ae51bd264d85411f2
caroscaj <caroscaj@mail.uc.edu>
2022-10-18 15:00:30 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int n= 30;
+        while ( n >= 0)
+        {
+            System.out.println(n + " ");
+            n--;
+        }
+    }
+}
